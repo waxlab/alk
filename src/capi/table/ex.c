588db01489dc80be3131e788bdfc74d058ec1e10
Thadeu de Paula <thadeudepaula@gmail.com>
2022-10-03 13:14:42 -0300

    transition to simpler structure

--- src/capi/table/ex.c
+++ src/capi/table/ex.c
@@ -2,8 +2,14 @@
 #include <lauxlib.h>
 #include <string.h>
 #include "../capi.h"
+#include "luaconf.h"
+#include "stdlib.h"
 
-static int keys(lua_State *L) {
+/*
+** as
+** capi.table.c.keys
+*/
+static int module_keys(lua_State *L) {
   char keys[100][100];
   int res = 0;
   int i;
@@ -21,6 +27,12 @@ static int keys(lua_State *L) {
   for (i=0; i<res; i++) lua_pushstring(L, keys[i]);
   return res;
 }
+
+
+/*
+** as
+** capi.table.c.keysrec
+*/
 
 typedef struct {
   int c;
@@ -44,7 +56,12 @@ static void getkeys(lua_State *L, int idx, recurse_res *res) {
 }
 
 
-static int krecursive(lua_State *L) {
+/*
+** as
+** capi.table.c.keys
+*/
+
+static int module_keysrec(lua_State *L) {
   recurse_res res = { 0, {{0}}};
   int i;
   getkeys(L, 1, &res);
@@ -53,24 +70,62 @@ static int krecursive(lua_State *L) {
 }
 
 
-static int vals(lua_State *L) {
-  int count = 0;
-  /* Check if argument is a Lua table */
-  if (lua_type(L,1) == LUA_TTABLE) {
-    while( lua_next(L,1) ) {
-      /* values are always under -2 when a table is on top of stack */
-      count++;
+
+
+static int module_values(lua_State *L) {
+  int type;
+  int mslot = 1;
+  int datalen = 0;
+  int reslen = 0;
+
+  /* As example we treat only values if they are string or number. */
+  typedef struct {
+    int type;
+    union { double number; const char *string;} value;
+  } resitem;
+  resitem *res = malloc(sizeof(resitem));
+
+  lua_pushnil(L);
+  if (lua_type(L,-2) == LUA_TTABLE) {
+    while( lua_next(L,-2) != 0 ) {
+      type = lua_type(L,-1);
+
+      if (datalen == mslot) {
+        /* Allocate memory when needed */
+        if ( (res = realloc(res, sizeof(resitem) * ((mslot *= 2 )))) == NULL ) {
+          lua_pushstring(L,"out of memory");
+          lua_error(L);
+        }
+      }
+
+      if (type == LUA_TSTRING) {
+        res[datalen].type = type;
+        res[datalen++].value.string = lua_tostring(L,-1);
+      } else if (type == LUA_TNUMBER) {
+        res[datalen].type = type;
+        res[datalen++].value.number = lua_tonumber(L,-1);
+      }
+      lua_pop(L,1);
     }
   }
-  return count;
+
+  for(; reslen < datalen; reslen++) {
+    /* We push what we collected in struct */
+    switch (res[reslen].type) {
+      case LUA_TNUMBER: lua_pushnumber(L, res[reslen].value.number); break;
+      case LUA_TSTRING: lua_pushstring(L, res[reslen].value.string); break;
+    }
+  }
+  free(res);
+  return reslen;
 }
 
 int luaopen_alk_capi_table_c(lua_State *L) {
 
   luaL_Reg module[] = {
-    { "keys", keys  },
-    { "vals", vals  },
-    { "krecursive", krecursive },
+    { "keys",    module_keys    },
+    { "values",  module_values  },
+    { "keysrec", module_keysrec },
     { NULL, NULL    }
   };
 
--- src/capi/table/ex.lua
+++ src/capi/table/ex.lua
@@ -1,20 +1,36 @@
 local t = require 'alk.capi.table.c'
 
-
+-- capi.table.values(t:{}) : {}
+-- Works like table.unpack but for any kind of tables.
+-- As table keys doesn't keep order, doesn't make much sense so.
 do
-  local a, b = t.keys {x = "b", y = 10}
+  local a,b = t.values {x=100, y="hi"}
+  print(a,b)
+  assert(
+    a == 100 and b == "hi"
+    or
+    a == "hi" and b == 100
+  )
+end
+
+
+-- capi.table.keys(t:{}) : string, ...
+-- Get keys only from first level
+do
+  local a, b = t.keys {x = "b", y = {z = 10}}
   -- The order may vary as tables are not sorted
   assert(a == "x" or a == "y")
   assert(b == "x" or b == "y")
 end
 
+-- capi.table.keysrec(t:{}) : string, ...
+-- Get keys recursively
 do
-  local a, b, c, d = t.krecursive { w = { x = { y = { z = 10}}}}
+  local a, b, c, d = t.keysrec { w = { x = { y = { z = 10}}}}
   assert(a == "w" and b == "x" and c == "y" and d == "z")
-end
 
-do
-  local a, b, c, d = t.krecursive { w = { x = 1}, y = { z=1 }}
+  -- In this case we check in different orders
+  local a, b, c, d = t.keysrec { w = { x=1 }, y = { z=1 }}
   assert(
     (a == "w" and b == "x" and c == "y" and d == "z")
     or
